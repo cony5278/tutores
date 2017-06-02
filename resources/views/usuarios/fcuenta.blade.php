546853bea8c29c138caceabdc51699dd068ad641
constanza <cony5278@outlook.com>
2017-06-01 22:49:59 -0500

    actualizacion

--- resources/views/usuarios/fcuenta.blade.php
+++ resources/views/usuarios/fcuenta.blade.php
@@ -1,13 +1,16 @@
 <div class="contenedor-izquierda  .col-xs-12 .col-SM-6 .col-lg-8">
-   {{ HTML::image('/app/public/logo.jpg', "Imagen", array('class'=>'img-circle img-perfil')) }}
+   {{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-perfil')) }}
    	<h5>Juan camilo rodriguez</h5>
    	 <div class="group">      
       <input class="buscar" type="text" required>
       <span class="highlight"></span>
       <span class="bar"></span>
-      <label>buscar publicación</label>
+      <label class="lbuscar">buscar publicación</label>
     </div>
-    <a>confi</a>
+    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
+    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
+    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
+  	<a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
 </div>
 </div>
 <div class="contenedor-cabeza .col-xs-12 .col-SM-6 .col-lg-8">
