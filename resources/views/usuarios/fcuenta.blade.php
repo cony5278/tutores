d48a0f8aff44dfe17342d66c9418b36fe6a9b8b7
constanza <cony5278@outlook.com>
2017-06-04 21:04:36 -0500

    menu lateral de la cuenta del usuario

--- resources/views/usuarios/fcuenta.blade.php
+++ resources/views/usuarios/fcuenta.blade.php
@@ -1,25 +1,13 @@
 <div class="contenedor-cuenta">
-  <div class="contenedor-perfil col-xs-12 col-sm-12 col-md-4">
+  <div class="contenedor-perfil ">
     @include('usuarios/fperfil')
+
   </div>
-
-
 
   <div class="contenedor-izquierda  col-xs-2 col-sm-2 col-md-2">
-     {{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-perfil')) }}
-     	<h5>Juan camilo rodriguez</h5>
-  	   	 <div class="group">      
-  		      <input class="buscar" type="text" required>
-  		      <span class="highlight"></span>
-  		      <span class="bar"></span>
-  		      <label class="lbuscar">buscar publicación</label>
-  	    </div>
-  	    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config','id'=>'img-cuenta-config')) }}</a>
-  	    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
-  	    <a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
-  	  	<a>{{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-config')) }}</a>
+      @include('usuarios/menu')
   </div>
-
+  
   <div class="configuracion-usuario ">
     		<div class="configuracion-usuario-izquierda col-xs-10 col-sm-10 col-md-6">
     			izquierda
---
+++ resources/views/usuarios/menu.blade.php
@@ -0,0 +1,36 @@
+
+<div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+             <a class="navbar-brand" href="#">
+                {{ HTML::image('../img/logo.jpg', "Imagen", array('class'=>'img-circle img-perfil')) }}
+               <h5>Juan camilo rodriguez</h5>
+            </a>
+        </div>
+</div>
+</br>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1" >
+   <div class="group">      
+           <input class="buscar" type="text" required>
+           <span class="highlight"></span>
+           <span class="bar"></span>
+           <label class="lbuscar">buscar publicación</label>
+    </div>
+    <ul>
+        <li><a class="img-config">sdf</a></li>
+        <li><a>sdf</a></li>
+        <li><a>sdf</a></li>
+        <li><a>sdf</a></li>                 
+    </ul>
+
+</div>
+
+
+
